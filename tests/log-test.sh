f5bdcf87a7d2c6857e91115299ce9f7c39611767
francesco <info.francescobianco@gmail.com>
2024-11-13 17:20:06 +0100

    Fix test

--- tests/log-test.sh
+++ tests/log-test.sh
@@ -9,4 +9,4 @@
 #curl -L -d ci2a2o https://script.google.com/macros/s/AKfycbzNiUWPUKKG_ZVaYfcKjMyLdElpsu92FhKS7BdZTQYzUIKtm8DlLGKmlk2X8m_QYyy-JA/exec
 
 
-curl -d ciao log.yafb.net -H SERVICE:backup -H LOGKEY:AKfycbzNiUWPUKKG_ZVaYfcKjMyLdElpsu92FhKS7BdZTQYzUIKtm8DlLGKmlk2X8m_QYyy-JA
+curl -L https://log.yafb.net -H service:backup2 -H logkey:$LOGKEY -d ciao
