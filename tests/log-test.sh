dae7abce899c72134073c265f6a064aa98d58e8d
francesco <info.francescobianco@gmail.com>
2024-11-13 16:52:22 +0100

    Release

--- tests/log-test.sh
+++ tests/log-test.sh
@@ -9,4 +9,4 @@
 #curl -L -d ci2a2o https://script.google.com/macros/s/AKfycbzNiUWPUKKG_ZVaYfcKjMyLdElpsu92FhKS7BdZTQYzUIKtm8DlLGKmlk2X8m_QYyy-JA/exec
 
 
-curl -L -XPOST log.yafb.net -H LOGKEY:AKfycbzNiUWPUKKG_ZVaYfcKjMyLdElpsu92FhKS7BdZTQYzUIKtm8DlLGKmlk2X8m_QYyy-JA
+curl -d ciao log.yafb.net -H LOGKEY:AKfycbzNiUWPUKKG_ZVaYfcKjMyLdElpsu92FhKS7BdZTQYzUIKtm8DlLGKmlk2X8m_QYyy-JA
