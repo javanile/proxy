3c260efaea1bc77016b0c686c22e56b2e629dd27
francesco <info.francescobianco@gmail.com>
2024-11-13 16:25:34 +0100

    Release

--- tests/log-test.sh
+++ tests/log-test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-curl log.yafb.net
+#
+
+#curl -XPOST -L -d cia2o
+
+
+#curl -L https://script.google.com/macros/s/AKfycbzNiUWPUKKG_ZVaYfcKjMyLdElpsu92FhKS7BdZTQYzUIKtm8DlLGKmlk2X8m_QYyy-JA/exec
+#curl -L -d ci2a2o https://script.google.com/macros/s/AKfycbzNiUWPUKKG_ZVaYfcKjMyLdElpsu92FhKS7BdZTQYzUIKtm8DlLGKmlk2X8m_QYyy-JA/exec
+
+
+curl -XPOST log.yafb.net -d "Hello, world!" -H LOGKEY:AKfycbzNiUWPUKKG_ZVaYfcKjMyLdElpsu92FhKS7BdZTQYzUIKtm8DlLGKmlk2X8m_QYyy-JA
