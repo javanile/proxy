4388208e9d0d97a7c2726bcb894daf293ab4e2c2
francesco <info.francescobianco@gmail.com>
2025-04-11 19:00:52 +0200

    Release

--- cgi-bin/google-script.sh
+++ cgi-bin/google-script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+debug=
 headers=$(mktemp)
 body=$(mktemp)
 remote_host=$(echo "$REMOTE_ADDR" | cut -d: -f1)
@@ -9,18 +10,18 @@ query=$QUERY_STRING
 path=$PATH_INFO
 url="https://script.google.com/macros/s/${deployment_id}/exec?\$REMOTE_ADDRESS=${remote_host}&\$USER_AGENT=${user_agent}&\$REQUEST_URI=${path}&${query}"
 
-
 if [ "$REQUEST_METHOD" != "POST" ]; then
-  curl -s -L -D "$headers" -o "$body" \
-    -X GET \
-    "${url}"
+  curl -s -L -D "$headers" -o "$body" -X GET "${url}"
 else
-  curl -L "${url}" -D "$headers" -o "$body" \
-    -H 'Host: script.google.com' -H 'User-Agent: SOMETHING' \
+  curl -L "${url}" --data @- --compressed -D "$headers" -o "$body" \
+    -H 'Host: script.google.com' \
+    -H 'User-Agent: SOMETHING' \
     -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
-    -H 'Accept-Language: en,en-US;q=0.5' --compressed \
-    -H 'Referer: SOMETHING' -H 'DNT: 1' -H 'Connection: keep-alive' \
-    -H 'Upgrade-Insecure-Requests: 1' --data @-
+    -H 'Accept-Language: en,en-US;q=0.5' \
+    -H 'Referer: SOMETHING' \
+    -H 'DNT: 1' \
+    -H 'Connection: keep-alive' \
+    -H 'Upgrade-Insecure-Requests: 1'
 fi
 
 exit_code=$?
@@ -32,17 +33,19 @@ fi
 
 header_stop=$(grep -n '^HTTP/' "$headers" | tail -n 1 | cut -d: -f1)
 
-echo ""
-echo "URL:"
-echo "$url"
-echo "ENV:"
-printenv
-echo "HEADERS:"
-cat "$headers"
-echo "BODY:"
-cat "$body"
-
-sed -n "$((header_stop+1)),\$p" "$headers"
-cat "$body"
+if [ -z "$debug" ]; then
+  sed -n "$((header_stop+1)),\$p" "$headers"
+  cat "$body"
+else
+  echo ""
+  echo "URL:"
+  echo "$url"
+  echo "ENV:"
+  printenv
+  echo "HEADERS:"
+  cat "$headers"
+  echo "BODY:"
+  cat "$body"
+fi
 
 rm -f "$headers" "$body" >/dev/null 2>&1
